1855886c4553bc5f4e98090f3a909c2701ccae84
Jean Disset <j.disset@gmail.com>
2024-06-26 00:44:15 -0400

    More modular loaders

--- dracon/composer.py
+++ dracon/composer.py
@@ -1,76 +1,97 @@
 from ruamel.yaml import YAML
 from ruamel.yaml.composer import Composer
-from ruamel.yaml.nodes import ScalarNode
+from ruamel.yaml.nodes import MappingNode, SequenceNode, ScalarNode
+from ruamel.yaml.nodes import ScalarNode, Node
 from ruamel.yaml.events import (
     AliasEvent,
     ScalarEvent,
+    SequenceStartEvent,
+    MappingStartEvent,
 )
 from .merge import MergeKey, merged
 from .utils import dict_like
-from .loader import load_raw_conf_str
+
+class IncludeNode(ScalarNode):
+
+    def __init__(self, value, start_mark=None, end_mark=None, tag=None, anchor=None, comment=None):
+        if tag is None:
+            tag = 'dracon_include'
+        ScalarNode.__init__(self, tag, value, start_mark, end_mark, comment=comment, anchor=anchor)
+        # TODO: check that its a valid include, and if not print a pretty error using start_mark
+        
+
+def keypath_str(keypath):
+    out_str = ''
+    for i, p in enumerate(keypath):
+        if i <= 1:
+            out_str += p
+        else:
+            out_str += '.' + p
+    return out_str
 
 class DraconComposer(Composer):
-    def compose_node(self, parent, index):
-        event = self.parser.peek_event()
 
-        if self.parser.check_event(ScalarEvent):
-            if event.style is None and MergeKey.is_merge_key(event.value):
-                event.tag = 'dracon_merge'
-            return super().compose_node(parent, index)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.path_stack = []
+        self.anchor_paths = {}
+
+    def descend_path(self, parent, index):
+        if parent is None:
+            self.path_stack.append('/')
+        elif isinstance(parent, MappingNode):
+            if index is None:
+                self.path_stack.append(None)
+            elif isinstance(index, ScalarNode):
+                self.path_stack.append(index.value)
+            else:
+                self.path_stack.append(str(index))
+        elif isinstance(parent, SequenceNode):
+            self.path_stack.append(str(index))
+
+    def ascend_path(self):
+        if self.path_stack:
+            self.path_stack.pop()
+
+    def get_path(self):
+        return [p for p in self.path_stack if p is not None]
+
+    def compose_node(self, parent, index):
+        self.descend_path(parent, index)
+        current_path = self.get_path()
 
         if self.parser.check_event(AliasEvent):
             event = self.parser.get_event()
-            alias = event.anchor
-            if alias in self.anchors:
-                return self.return_alias(self.anchors[alias])
+            node = IncludeNode(
+                value=current_path,
+                start_mark=event.start_mark,
+                end_mark=event.end_mark,
+                anchor=event.anchor,
+            )
+        else:
+            event = self.parser.peek_event()
+            anchor = event.anchor
+            if anchor is not None:
+                assert anchor not in self.anchor_paths, f'Anchor {anchor} already exists'
+                self.anchor_paths[anchor] = keypath_str(current_path)
+
+            self.resolver.descend_resolver(parent, index)
+            if self.parser.check_event(ScalarEvent):
+                if event.style is None and MergeKey.is_merge_key(event.value):
+                    event.tag = 'dracon_merge'
+                node = self.compose_scalar_node(anchor)
+            elif self.parser.check_event(SequenceStartEvent):
+                node = self.compose_sequence_node(anchor)
+            elif self.parser.check_event(MappingStartEvent):
+                node = self.compose_mapping_node(anchor)
             else:
-                return ScalarNode(
-                    tag='dracon_include',
-                    value=event.anchor,
-                    start_mark=event.start_mark,
-                    end_mark=event.end_mark,
-                )
+                raise RuntimeError(f'Not a valid node event: {event}')
 
-        return super().compose_node(parent, index)
+            self.resolver.ascend_resolver()
 
-def perform_merges(conf_obj):
-    if isinstance(conf_obj, list):
-        return [perform_merges(v) for v in conf_obj]
+            if anchor:
+                self.anchor_paths[anchor] = keypath_str(current_path)
 
-    if dict_like(conf_obj):
-        res = {}
-        merges = []
-        for key, value in conf_obj.items():
-            if hasattr(key, 'tag') and key.tag == 'dracon_merge':
-                merges.append((MergeKey(raw=key.value), value))
-            else:
-                res[key] = perform_merges(value)
-        for merge_key, merge_value in merges:
-            res = merged(res, merge_value, merge_key)
-        return res
-
-    return conf_obj
-
-
-def resolve_includes(conf_obj, base_path=None):
-    if dict_like(conf_obj):
-        return {k: resolve_includes(v, base_path) for k, v in conf_obj.items()}
-    if isinstance(conf_obj, list):
-        return [resolve_includes(v, base_path) for v in conf_obj]
-    if hasattr(conf_obj, 'tag') and conf_obj.tag == 'dracon_include':
-        return load_yaml(load_raw_conf_str(conf_obj.value))
-    return conf_obj
-
-
-def dracon_post_process(loaded):
-    loaded = resolve_includes(loaded)
-    loaded = perform_merges(loaded)
-    return loaded
-
-def load_yaml(content: str):
-    yaml = YAML()
-    yaml.preserve_quotes = True
-    yaml.Composer = DraconComposer
-    loaded_raw = yaml.load(content)
-    return dracon_post_process(loaded_raw)
+        self.ascend_path()
+        return node
 
--- dracon/loader.py
+++ dracon/loader.py
@@ -1,88 +1,229 @@
-import re
+## {{{                       --     imports & doc     --
+from ruamel.yaml import YAML
+import os
+from typing import Type
+import importlib.resources
+from importlib.resources import files, as_file
 from pathlib import Path
 from typing import Optional, Dict, Any
 from pydantic import BaseModel
-from dracon.utils import dict_like
+from dracon.utils import dict_like, get_obj_at_keypath
+from dracon.merge import perform_merges
+from dracon.composer import IncludeNode, DraconComposer
+import importlib.resources
+from importlib.resources import files, as_file
 
 """
-    Dracon allows for including external configuration files in the YAML configuration files. The include paths can be specified in the following formats:
-    pkg:pkg_name:config_path[@keypath]
-    file:config_path[@keypath]
+    Dracon allows for including external configuration files in the YAML configuration files.
+    Dracon provides 3 default loaders:
 
-    @keypath is optional and is used to specify a subpath within the included dictionary.
+    pkg: for including files from a python package
+    file: for including files from the filesystem
+    env: for including environment variables
+
+    special loaders (no need for ':'):
+    / root of the current document
+    @ current scope
+    ..(n) parent scope (n times)
+
+    syntax:
+    loader:[path:][@key.path]
+
     a path can be specified with or without the .yaml extension.
 
+    @key.path is optional and is used to specify a subpath within the included dictionary.
+    in a keypath, dot notation is used to specify the path to the key within the dictionary.
+
+    When an include references another included item, we build a tree of dependencies and resolve them in topological order (bottom-up).
+
+    In theory, even though it's most likely a bad idea, nothing prevents you from defining anchors that have ambiguous names (names that look like loader paths, that start with '/', '.' or '@' or contain ':' ).
+    Existing regular anchors will take precedence over the special loader paths.
+
+    ## examples:
+
+    ```
+    default_settings:
+        setting1: default_value1
+        dict2: &alias
+            subkey1: value1
+        from_pkg: &pkgalias
+            <<: *pkg:dracon:tests/configs/params # using the merge syntax (would be the same to directly include as value)
+        from_file: *file:./configs/params.yaml@subkey1 # include the value of subkey1 from the file (uses a relative path to the current file)
+        from_file: *file:use_executable_path:./configs/params.yaml@subkey1 # include the value of subkey1 from the file (uses a relative path to the executing script)
+
+    settings:
+
+        # @ and leading . mean local parent scope. Both can be omitted (but be careful with name collisions if you have aliases that use these characters)
+        # / means root of the current document
+        # extra dots mean going up in the hierarchy
+
+        - */default_settings.setting1 # will include default_value1
+        - */.@.@.default_settings.setting1 # same
+        - *..default_settings.setting1 # same
+
+
+        - *@0 # will repeat the first item of this list (scope of @ is the current obj) (first dot is implicit)
+        - *.0 # same
+
+        - *.1 # will repeat the second item of this list after it is resolved, i.e it'll be default_value1
+
+        - *env:HOME # include the value of the HOME environment variable
+
+        - *alias # dumps the entire dict2 (the "vanilla" alias behavior)
+        - *alias@ # same
+        - *alias@subkey1 # dumps value1
+        - *alias@.subkey1 # same
+        # !!!! - *alias.subkey1 WON'T WORK as you expect. It'll just be interpreted as an anchor name. You need to use @ to specify a subpath.
+
+        - *alias@/default_settings.setting1 # dumps default_value1. Syntaxically valid but not very useful
+        - *alias@..setting1 # same but using relative path to &alias
+
+        - *pkgalias # dumps the entire dict from the package
+
+    ```
+
 """
 
-class ConfPathLoader(BaseModel):
 
-    def load_raw(self, path: str):
-        raise NotImplementedError
-
-    def with_yaml_ext(self, path: str) -> str:
-        if not path.endswith('.yaml'):
-            return path + '.yaml'
-        return path
+##────────────────────────────────────────────────────────────────────────────}}}
 
 
-class FileConfPathLoader(ConfPathLoader):
-    def load_raw(self, path: str):
-        p = Path(self.with_yaml_ext(path)).expanduser().resolve()
-        if not p.exists():
-            raise FileNotFoundError(f'File not found: {p}')
-        with open(p, 'r') as f:
-            return f.read()
-
-
-class PkgConfPathLoader(ConfPathLoader):
-    def load_raw(self, path: str):
-        import importlib.resources
-        from importlib.resources import files, as_file
-
-        pkg = __name__
-        if ':' in path:
-            pkg, path = path.split(':', 1)
-
-        fpath = self.with_yaml_ext(path)
-        try:
-            with as_file(files(pkg) / fpath) as p:
-                with open(p, 'r') as f:
-                    return f.read()
-        except FileNotFoundError:
-            raise FileNotFoundError(f'File not found in package {pkg}: {fpath}')
-
-
-def load_raw_conf_str(path: str, escape_keys=None) -> str:
-    ctype = 'file'
-    cpath = path
-    raw_yaml = None
-    if ':' in path:
-        ctype, cpath = path.split(':', 1)
-    if ctype == 'file':
-        raw_yaml = FileConfPathLoader().load_raw(cpath)
-    elif ctype == 'pkg':
-        raw_yaml = PkgConfPathLoader().load_raw(cpath)
-    else:
-        raise ValueError(f'Unknown include type: {ctype}')
-    if escape_keys:
-
-        def replace_merge(match):
-            return match.group(0).replace(match.group(1), '"' + match.group(1) + '"')
-
-        for key in escape_keys:
-            pattern = re.compile(r'^\s*(' + key + r')\s*:\s*$', re.MULTILINE)
-            raw_yaml = pattern.sub(replace_merge, raw_yaml)
-    return raw_yaml
-
-
-class IncludePath(BaseModel):
-    path: str
-    subpath: Optional[str] = None
+class IncludeAlias(BaseModel):
+    mainpath: str
+    keypath: Optional[str] = None
 
     def model_post_init(self, *args, **kwargs):
         super().model_post_init(*args, **kwargs)
-        if '@' in self.path:
-            assert self.path.count('@') == 1, 'Only one @ is allowed in include path'
-            self.path, self.subpath = self.path.split('@', 1)
+        if '@' in self.mainpath:
+            assert self.mainpath.count('@') == 1, 'Only one @ is allowed in include path'
+            self.mainpath, self.keypath = self.mainpath.split('@', 1)
 
 
+def with_possible_ext(path: str):
+    p = Path(path)
+    # return: the original, with .yaml, with .yml, without extension. in that order
+    return [p, p.with_suffix('.yaml'), p.with_suffix('.yml'), p.with_suffix('')]
+
+
+def load_from_file(path: str, extra_paths=None):
+    all_paths = with_possible_ext(path)
+    if not extra_paths:
+        extra_paths = []
+
+    extra_path = [Path('./')] + [Path(p) for p in extra_paths]
+
+    for ep in extra_path:
+        for p in all_paths:
+            p = ep / p
+            if Path(p).exists():
+                path = p.as_posix()
+                break
+
+    p = Path(path)
+    if not p.exists():
+        raise FileNotFoundError(f'File not found: {path}')
+
+    with open(p, 'r') as f:
+        raw = f.read()
+    return load_config_from_str(raw)
+
+
+def load_from_pkg(path: str):
+    pkg = __name__
+
+    if ':' in path:
+        pkg, path = path.split(':', 1)
+
+    all_paths = with_possible_ext(path)
+
+    for fpath in all_paths:
+        try:
+            with as_file(files(pkg) / fpath.as_posix()) as p:
+                with open(p, 'r') as f:
+                    return load_config_from_str(f.read())
+        except FileNotFoundError:
+            pass
+
+    raise FileNotFoundError(f'File not found in package {pkg}: {path}')
+
+
+def load_from_env(path: str):
+    return os.getenv(path)
+
+
+DEFAULT_LOADERS = {
+    'file': load_from_file,
+    'pkg': load_from_pkg,
+    'env': load_from_env,
+}
+
+
+def load_from_include_str(
+    include_str: str,
+    path_to_node: str,
+    conf_obj: Any,
+    anchors=None,
+    custom_loaders: dict = DEFAULT_LOADERS,
+) -> Any:
+
+    if anchors is None:
+        anchors = {}
+
+    if '@' in include_str:
+        mainpath, keypath = include_str.split('@', 1)
+    else:
+        mainpath, keypath = include_str, ''
+
+    if path_to_node.startswith('/'):
+        path_to_node = path_to_node[1:]
+
+    # Handle special cases for relative paths
+    if mainpath.startswith('/'):
+        assert keypath == '', 'Key path not allowed for root include'
+        return get_obj_at_keypath(conf_obj, keypath[1:])
+
+    if mainpath.startswith('@') or mainpath.startswith('.'):
+        current_obj = get_obj_at_keypath(conf_obj, path_to_node)
+        return get_obj_at_keypath(current_obj, keypath[1:])
+
+    if mainpath in anchors:
+        refpath = anchors[mainpath]
+        obj = get_obj_at_keypath(conf_obj, refpath)
+        return get_obj_at_keypath(obj, keypath)
+
+    assert ':' in mainpath, f'Invalid include path: {mainpath} not in anchors: {anchors}'
+
+    loader, path = mainpath.split(':', 1)
+    if loader not in custom_loaders:
+        raise ValueError(f'Unknown loader: {loader}')
+
+    obj = custom_loaders[loader](path)
+    return get_obj_at_keypath(obj, keypath)
+
+
+
+def resolve_includes(conf_node, full_conf, anchor_paths=None):
+    if dict_like(conf_node):
+        return {k: resolve_includes(v, full_conf, anchor_paths) for k, v in conf_node.items()}
+    if isinstance(conf_node, list):
+        return [resolve_includes(v, full_conf, anchor_paths) for v in conf_node]
+    if hasattr(conf_node, 'tag') and conf_node.tag == 'dracon_include':
+        include_str = conf_node.anchor.value
+        keypath = conf_node.value
+        return load_from_include_str(include_str, keypath, full_conf, anchor_paths)
+    return conf_node
+
+
+def dracon_post_process(loaded, anchor_paths=None):
+    loaded = resolve_includes(loaded, loaded, anchor_paths)
+    loaded = perform_merges(loaded)
+    return loaded
+
+
+def load_config_from_str(content: str):
+    yaml = YAML()
+    yaml.preserve_quotes = True
+    yaml.Composer = DraconComposer
+    loaded_raw = yaml.load(content)
+    anchor_paths = yaml.composer.anchor_paths
+    return dracon_post_process(loaded_raw, anchor_paths)
