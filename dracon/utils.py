542f0f4bc10f772dc3accb4d944f2e12dc8189c8
Jean Disset <j.disset@gmail.com>
2024-07-01 15:37:03 -0400

    better dict_like and list_like utils

--- dracon/utils.py
+++ dracon/utils.py
@@ -1,95 +1,101 @@
 import xxhash
+from ruamel.yaml.nodes import MappingNode, SequenceNode, ScalarNode, Node
 import base64
-from typing import Any, Optional, Set, Sequence
+from typing import Iterable, TypeVar, Type, Tuple, Generic
+from typing import Protocol, runtime_checkable
+
+
+K = TypeVar('K')
+V = TypeVar('V')
+E = TypeVar('E')
+
+
+@runtime_checkable
+class DictLike(Protocol[K, V]):
+    def keys(self) -> Iterable[K]:
+        ...
+    def values(self) -> Iterable[V]:
+        ...
+    def items(self) -> Iterable[Tuple[K, V]]:
+        ...
+    def __getitem__(self, key: K) -> V:
+        ...
+    def __contains__(self, key: K) -> bool:
+        ...
+    def __setitem__(self, key: K, value: V) -> None:
+        ...
 
 def dict_like(obj) -> bool:
-    return (
-        hasattr(obj, 'keys')
-        and hasattr(obj, 'get')
-        and hasattr(obj, '__getitem__')
-        and hasattr(obj, '__contains__')
-        and hasattr(obj, '__iter__')
-        and hasattr(obj, 'items')
-    )
+    return isinstance(obj, DictLike)
+
+@runtime_checkable
+class ListLike_Permissive(Protocol[E]):
+    def __getitem__(self, index: int) -> E:
+        ...
+
+    # can be concatenated with another list-like object:
+    def __add__(self, other: 'ListLike_Permissive[E]') -> 'ListLike_Permissive[E]':
+        ...
+
+def permissive_list_like(obj) -> bool:
+    return isinstance(obj, ListLike_Permissive)
 
 
-def simplify_path(path: str):
-    # a path is a string starting with '/'
-    # then each part is separated by '.'
-    # 2 consecutive '.' means go up one level, 3 means go up 2 levels, etc.
-    # this function simplifies a path by removing the '..' and checks if it is in available_paths
+class ListLikeMeta(type):
+    def __instancecheck__(cls, instance):
+        return permissive_list_like(instance) and not dict_like(instance)
 
-    if not path.startswith('/'):
-        path = '/' + path
+class ListLike(Generic[E], metaclass=ListLikeMeta):
+    pass
 
-    # cut at last /
-    path = path[path.rfind('/') :]
+    def __getitem__(self, index: int) -> E:
+        ...
 
-    path = path[1:]  # remove leading '/'
-
-    # remove one trailing '.' if present
-    if path.endswith('.'):
-        path = path[:-1]
-    # if path is not in available_paths, try to simplify it
-    parts = path.split('.')
-    simplified = []
-    for part in parts:
-        if part == '':
-            if simplified:
-                simplified.pop()
-        else:
-            simplified.append(part)
-
-    simplified_path = '/' + '.'.join(simplified)
-
-    return simplified_path
+    def __add__(self, other: 'ListLike[E]') -> 'ListLike[E]':
+        ...
 
 
-def combine_paths(paths: Sequence[str]):
-    sp = simplify_path('/' + '.'.join(paths))
-    assert sp is not None
-    return sp
 
+def list_like(obj) -> bool:
+    return isinstance(obj, ListLike)
+
+
+# def list_like(obj):
+    # try:
+        # iter(obj)  # Check if the object is iterable
+        # obj[0]    # Check if the object supports indexing
+    # except (TypeError, KeyError, IndexError) as e:
+        # return isinstance(e, IndexError)
+    # return True
 
 
 def with_indent(content: str, indent: int) -> str:
-    return '\n'.join([f'{" " * indent}{line}' for line in content.split('\n')])
+    # replace all \n with \n + indent*' ', ONLY if the line is not empty
+    return '\n'.join([(indent*' ' + line) if line else '' for line in content.split('\n')])
+
 
 def get_hash(data: str) -> str:
     hash_value = xxhash.xxh128(data).digest()
     return base64.b32encode(hash_value).decode('utf-8').rstrip('=')
 
-
-
-def obj_get(obj: Any, attr: str):
-    """
-    Get an attribute from an object, handling various types of objects.
-    """
-    if isinstance(obj, list):
-        return obj[int(attr)]
-    if hasattr(obj, attr):
-        return getattr(obj, attr)
+def node_print(node: Node, indent_lvl=0, indent=2) -> str:
+    out = ''
+    if isinstance(node, MappingNode):
+        out += f'{type(node)}'+' {\n'
+        for key, value in node.value:
+            if hasattr(key, 'value'):
+                out += with_indent(f'{key.value}: {node_print(value, indent_lvl+indent)}', indent_lvl)
+            else:
+                out += with_indent(f'noval(<{type(key)}>{key}): {node_print(value, indent_lvl+indent)}', indent_lvl)
+        out += '},\n'
+    elif isinstance(node, SequenceNode):
+        indent_lvl += indent
+        out += f'{type(node)}'+' [\n'
+        for value in node.value:
+            out += with_indent(node_print(value, indent_lvl+indent), indent_lvl)
+        out += '],\n'
+    elif isinstance(node, ScalarNode):
+        out += f'{node.value},\n'
     else:
-        try:  # check if we can access it with __getitem__
-            return obj[attr]
-        except (TypeError, KeyError):
-            raise AttributeError(f'Could not find attribute {attr} in {obj}')
-
-# TODO: allow backtracking with consecutive dots
-def get_obj_at_keypath(obj: Any, attr_path: str):
-
-    if attr_path.startswith('/'):
-        return get_obj_at_keypath(obj, attr_path[1:])
-
-    if attr_path == '':
-        return obj
-
-    res = obj
-    for attr in attr_path.split('.'):
-        try:
-            res = obj_get(res, attr)
-        except (AttributeError, KeyError, IndexError) as e:
-            raise AttributeError(f'Could not find path {attr_path} in {type(obj)} instance: {e}')
-    return res
-
-
+        out += f'<{type(node)}> {node.value},\n'
+    return out
