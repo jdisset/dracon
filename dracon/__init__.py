e8dc01791859f3106683a344822162ced6b273d8
Jean Disset <j.disset@gmail.com>
2024-07-02 20:35:14 -0400

    cleaner __init__

--- dracon/__init__.py
+++ dracon/__init__.py
@@ -8,16 +8,3 @@ from dracon.merge import *
 from dracon.loader import *
 from dracon.composer import *
 from dracon.keypath import *
-
-# cfg = load_config_from_str(load_raw_conf_str('pkg:dracon:tests/configs/simple.yaml'))
-# cfg = load_from_include_str('pkg:dracon:tests/configs/simple.yaml', '', None)
-# cfg = load_from_pkg('dracon:tests/configs/simple.yaml')
-
-# # dump using ruamel.yaml:
-# from ruamel.yaml import YAML
-# import io
-# yaml = YAML()
-# str_stream = io.StringIO()
-# yaml.dump(cfg, str_stream)
-# print(str_stream.getvalue())
-
