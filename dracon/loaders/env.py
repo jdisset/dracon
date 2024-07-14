f4e28d84808ba291753582ca7663fc3d400ad357
Jean Disset <jdisset@mit.edu>
2024-07-14 17:30:06 -0400

    new loader class

--- dracon/loaders/env.py
+++ dracon/loaders/env.py
@@ -3,6 +3,9 @@ from typing import ForwardRef, TypeAlias
 
 DraconLoader = ForwardRef('DraconLoader')
 
+def read_from_env(path: str):
+    return str(os.getenv(path))
+
 def compose_from_env(path: str, loader: DraconLoader):
-    return loader.compose_config_from_str(str(os.getenv(path)))
+    return loader.compose_config_from_str(read_from_env(path))
 
