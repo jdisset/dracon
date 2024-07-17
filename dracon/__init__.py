898645819c434e8f71ab53b5095608db96e3e198
Jean Disset <jdisset@mit.edu>
2024-07-17 15:29:16 -0400

    raw dict load for dracon

--- dracon/__init__.py
+++ dracon/__init__.py
@@ -5,12 +5,16 @@ from .composer import *
 from .keypath import *
 
 
-def load(config_path: str | Path):
+def load(config_path: str | Path, raw_dict=False):
     loader = DraconLoader()
+    if raw_dict:
+        loader.yaml.constructor.yaml_base_dict_type = dict
     return loader.load(config_path)
 
-def loads(config_str: str):
+def loads(config_str: str, raw_dict=False):
     loader = DraconLoader()
+    if raw_dict:
+        loader.yaml.constructor.yaml_base_dict_type = dict
     return loader.loads(config_str)
 
 def dump(data, stream=None):
