8ff2cb2502ccdf85f65676285e0d6d647446522e
Jean Disset <jdisset@mit.edu>
2026-03-09 16:25:52 -0400

    fix: propagate setdefault defined vars

--- dracon/tests/test_define_include.py
+++ dracon/tests/test_define_include.py
@@ -1,6 +1,7 @@
 import pytest
 from dracon.loader import DraconLoader
 from dracon.include import compose_from_include_str
+from dracon.deferred import DeferredNode
 
 
 def get_config(config_path):
@@ -21,3 +22,16 @@ def test_define_propagates_through_merge_include():
     config = get_config("dracon:tests/test_define_propagation_main.yaml")
     assert config["bar"] == 42  # from included file
     assert config["test"] == 43  # FOO + 1 using FOO from included file
+
+
+def test_setdefault_propagates_through_merge_include():
+    config = get_config("dracon:tests/test_setdefault_propagation_main.yaml")
+    assert config["greeting"] == "hello"
+    assert config["test"] == 43
+
+
+def test_setdefault_propagates_to_deferred_reroot():
+    config = get_config("dracon:tests/test_setdefault_deferred_main.yaml")
+    assert isinstance(config["result"], DeferredNode)
+    constructed = config["result"].construct()
+    assert constructed["value"] == 42
